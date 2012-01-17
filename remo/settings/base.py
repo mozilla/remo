09c7ea744d0068fb8630e7bc9be0d867491294cc
Giorgos Logiotatidis <seadog@sealabs.net>
2012-01-17 23:59:55 +0200

    Renamed project module to remo

--- remo/settings/base.py
+++ remo/settings/base.py
@@ -24,13 +24,13 @@ MINIFY_BUNDLES = {
 }
 
 # Defines the views served for root URLs.
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'remo.urls'
 
 INSTALLED_APPS = list(INSTALLED_APPS) + [
     # Application base, containing global templates.
-    'project.base',
+    'remo.base',
     # Example code. Can (and should) be removed for actual projects.
-    'project.examples',
+    'remo.examples',
 ]
 
 
