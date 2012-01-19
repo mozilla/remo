e2de101eb7fb46eabe35f2694c7954e08def14a3
Giorgos Logiotatidis <seadog@sealabs.net>
2012-01-19 15:58:49 +0200

    Change version number

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, the authors'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = '1.0'
 
