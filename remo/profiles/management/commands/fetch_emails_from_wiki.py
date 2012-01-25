333e5c22eaa100a37acde6e88a9948ed0e03da69
Giorgos Logiotatidis <seadog@sealabs.net>
2012-01-25 19:40:25 +0200

    Add script to fetch emails from wiki

---
+++ remo/profiles/management/commands/fetch_emails_from_wiki.py
@@ -0,0 +1,60 @@
+import sys
+import json
+
+import requests
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.core.validators import email_re
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Fetch ReMo emails from wiki.mozilla.org'
+    offset = 0
+    URL = 'https://wiki.mozilla.org/api.php?action=ask&'\
+          'q=[[Category:Remouser]]'\
+          '&format=json&offset=%s&po=bugzillamail'
+
+
+    def handle(self, *args, **options):
+        while self.offset > -1:
+            r = requests.get(self.URL % self.offset)
+
+            if r.status_code != 200:
+                print "Error fetching Wiki data"
+                sys.exit(-1)
+
+            try:
+                data = json.loads(r.text)
+
+            except ValueError, e:
+                print r.text
+                print "Error decoding Wiki data"
+                sys.exit(-1)
+
+            # convinience pointers
+            results = data['ask']['results']
+            query = data['ask']['query']
+
+            # check offset
+            if results.has_key('hasMore') and results['hasMore'] == 'true':
+                self.offset += int(results['count']) + 1
+
+            else:
+                self.offset = -1
+
+            for entry in results['items']:
+                try:
+                    email = entry['properties']['bugzillamail']
+
+                except KeyError:
+                    continue
+
+                # sanitize input
+                if not isinstance(email, basestring) or not email_re.match(email):
+                    # ignoring invalid email
+                    print "# Invalid email for %s" % entry['uri']
+                    continue
+
+                print email
