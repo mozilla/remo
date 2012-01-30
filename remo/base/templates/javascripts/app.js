b70bd50d0eabebc5a1354682acf596931463f5b0
Pierros Papadeas <ppapadeas@gmail.com>
2012-01-30 15:18:56 +0200

    People page and openlayers

--- remo/base/templates/javascripts/app.js
+++ remo/base/templates/javascripts/app.js
@@ -97,6 +97,22 @@ $(document).ready(function () {
 
 	/* DISABLED BUTTONS ------------- */
 	/* Gives elements with a class of 'disabled' a return: false; */
+
+
+	/* OpenLayers Map ------------- */
+
+        var map, layer;
+        function init(){
+            map = new OpenLayers.Map( 'map');
+            layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+            map.addLayer(layer);
+            map.setCenter(
+                new OpenLayers.LonLat(-71.147, 42.472).transform(
+                    new OpenLayers.Projection("EPSG:4326"),
+                    map.getProjectionObject()
+                ), 12
+            );    
+        }
   
 
 });
