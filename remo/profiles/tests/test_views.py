99adfc6bfb40720ab6f6ec0904452911bff9abfd
Giorgos Logiotatidis <seadog@sealabs.net>
2012-02-06 16:24:48 +0100

    Initial profiles.views testing

--- remo/profiles/tests/test_views.py
+++ remo/profiles/tests/test_views.py
@@ -16,3 +16,62 @@ class ViewsTest(TestCase):
         eq_(u.userprofile.added_by, User.objects.get(username="mentor"))
 
 
+    def test_edit_profile(self):
+        # user edits own profile
+        c = Client()
+        c.login(username="rep", password="passwd")
+        response = c.get('/u/koki/edit/', follow=True)
+        self.assertTemplateUsed(response, 'profiles_edit.html')
+
+        # admin edits user's profile
+        c = Client()
+        c.login(username="admin", password="passwd")
+        response = c.get('/u/koki/edit/', follow=True)
+        self.assertTemplateUsed(response, 'profiles_edit.html')
+
+        # third user denied permission to edit user's profile
+        c = Client()
+        c.login(username="mentor", password="passwd")
+        response = c.get('/u/koki/edit/', follow=True)
+        self.assertTemplateUsed(response, 'main.html')
+
+
+    def test_delete_profile(self):
+        # user can't delete own profile
+        c = Client()
+        c.login(username="rep", password="passwd")
+        response = c.post('/u/koki/delete/', follow=True)
+        self.assertTemplateUsed(response, 'main.html')
+        for m in response.context['messages']: pass
+        eq_(m.tags, u'error')
+
+        # admin can delete user's profile
+        c = Client()
+        c.login(username="admin", password="passwd")
+        response = c.post('/u/koki/delete/', {'delete':'true'}, follow=True)
+        self.assertTemplateUsed(response, 'main.html')
+        for m in response.context['messages']: m
+        eq_(m.tags, u'success')
+
+        # third user can't delete user's profile
+        c = Client()
+        c.login(username="mentor", password="passwd")
+        response = c.post('/u/koki/delete/', follow=True)
+        self.assertTemplateUsed(response, 'main.html')
+        for m in response.context['messages']: pass
+        eq_(m.tags, u'error')
+
+
+    def test_profiles_me(self):
+        # user gets own profile page rendered
+        c = Client()
+        c.login(username="rep", password="passwd")
+        response = c.get('/people/me/', follow=True)
+        self.assertTemplateUsed(response, 'profiles_view.html')
+
+        # anonymous user get message to login first
+        c = Client()
+        response = c.get('/people/me/', follow=True)
+        self.assertTemplateUsed(response, 'main.html')
+        for m in response.context['messages']: pass
+        eq_(m.tags, u'warning')
