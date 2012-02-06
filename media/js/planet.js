0a1ef3546a4e40ad2ea39c0f181e700e84219aca
Giorgos Logiotatidis <seadog@sealabs.net>
2012-02-06 16:40:17 +0100

    New layout of main page. Added planet feed

---
+++ media/js/planet.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#planet').FeedEk({
+		FeedUrl : 'http://planet.mozillareps.org/planet/posts/feeds/rss/',
+		MaxCount : 3,
+		ShowDesc : true,
+		ShowPubDate:false
+	});
+});
