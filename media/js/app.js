30510e30dc3fe3fcea1c3b7b24ca0a6549c58e7b
Giorgos Logiotatidis <seadog@sealabs.net>
2012-02-08 19:41:53 +0100

    Auto fadeout success messages

--- media/js/app.js
+++ media/js/app.js
@@ -99,20 +99,8 @@ $(document).ready(function () {
 	/* Gives elements with a class of 'disabled' a return: false; */
 
 
-	/* OpenLayers Map ------------- */
-
-        var map, layer;
-        function init(){
-            map = new OpenLayers.Map( 'map');
-            layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
-            map.addLayer(layer);
-            map.setCenter(
-                new OpenLayers.LonLat(-71.147, 42.472).transform(
-                    new OpenLayers.Projection("EPSG:4326"),
-                    map.getProjectionObject()
-                ), 12
-            );
-        }
+    // auto fade out success messages
+    $(".success").delay(5000).fadeOut('slow');
 
 
 });
