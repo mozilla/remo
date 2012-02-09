f91baadb9b0c848f3d0a81165ca781138a288d78
Giorgos Logiotatidis <seadog@sealabs.net>
2012-02-09 07:24:17 +0200

    Irc_name should not be blank

--- remo/profiles/models.py
+++ remo/profiles/models.py
@@ -96,7 +96,7 @@ class UserProfile(models.Model):
                                        validators=[_validate_twitter_username],
                                        blank=True)
     jabber_id = models.CharField(max_length=50, blank=True, default="")
-    irc_name = models.CharField(max_length=30, blank=True, default="")
+    irc_name = models.CharField(max_length=30, blank=False, default="")
     irc_channels = models.TextField(blank=True, default="")
     linkedin_url = models.URLField(blank=True, null=True, default="",
                                    validators=[_validate_linkedin_url])
