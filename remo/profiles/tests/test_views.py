59569fcafc1d2f84ae6254527c7246c3e25dae82
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-02-14 13:54:02 +0200

    Edit profile tests

--- remo/profiles/tests/test_views.py
+++ remo/profiles/tests/test_views.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 from nose.tools import eq_
 from django.test.client import Client
 from test_utils import TestCase
@@ -28,11 +31,11 @@ class ViewsTest(TestCase):
                      'irc_name': u'ircname',
                      'linkedin_url': u'http://www.linkedin.com/',
                      'facebook_url': u'http://www.facebook.com/',
-                     'diaspora_url': u'http://www.example.com',
-                     'personal_website_url': u'http://www.example.com',
-                     'personal_blog_feed': u'http://example.com',
+                     'diaspora_url': u'http://www.example.com/',
+                     'personal_website_url': u'http://www.example.com/',
+                     'personal_blog_feed': u'http://example.com/',
                      'bio': u'bio foo',
-                     'mentor_id': 1}
+                     'mentor': 6}
 
     def test_invite_user(self):
         c = Client()
@@ -79,6 +82,49 @@ class ViewsTest(TestCase):
         c.login(username="rep", password="passwd")
         response = c.post('/u/koki/edit/', self.data, follow=True)
         eq_(response.request['PATH_INFO'], '/people/me/')
+
+    def test_edit_profile(self):
+        c = Client()
+        c.login(username="rep", password="passwd")
+
+        # edit with correct data
+        response = c.post('/u/koki/edit/', self.data, follow=True)
+        with open("/tmp/ll.html", "w") as f:
+            f.write(response.content)
+        eq_(response.request['PATH_INFO'], '/people/me/')
+
+        # ensure that all user data was saved
+        user = User.objects.get(username="rep")
+        eq_(user.email, self.data['email'])
+        eq_(user.first_name, self.data['first_name'])
+        eq_(user.last_name, self.data['last_name'])
+
+        temp_data = self.data.copy()
+
+        # delete already checked items
+        eq_(user.userprofile.mentor.id, temp_data['mentor'])
+        eq_(user.userprofile.birth_date,
+            datetime.date(*(time.strptime(temp_data['birth_date'],
+                                          "%Y-%m-%d")[0:3])))
+        for item in ['email', 'first_name', 'last_name',
+                     'birth_date', 'mentor']:
+            del(temp_data[item])
+
+        # ensure that all user profile data was saved
+        for field in temp_data.keys():
+            eq_(getattr(user.userprofile, field), temp_data[field])
+
+        # test with missing mandatory fields
+        mandatory_fields = ['first_name', 'last_name', 'email',
+                            'birth_date', 'private_email', 'city', 'region',
+                            'country', 'lon', 'lat', 'mozillians_profile_url',
+                            'irc_name']
+        for field in mandatory_fields:
+            # remove a mandatory field and ensure that edit fails
+            temp_data = self.data.copy()
+            del(temp_data[field])
+            response = c.post('/u/koki/edit/', temp_data, follow=True)
+            self.assertTemplateUsed(response, 'profiles_edit.html')
 
     def test_delete_profile(self):
         # user can't delete own profile
