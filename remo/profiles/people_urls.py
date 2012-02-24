9fe8d1191f848497e5fe555896efb7552249597a
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-02-24 18:14:55 +0200

    Match /people/me and /people/invite to profiles_view_my_profile and profiles_invite accordingly.
    Fix bug 730261

--- remo/profiles/people_urls.py
+++ remo/profiles/people_urls.py
@@ -1,8 +1,22 @@
+from django.core.urlresolvers import reverse
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('remo.profiles.views',
-    url(r'^me/$', 'view_my_profile', name='profiles_view_my_profile'),
-    url(r'^invite/$', 'invite', name='profiles_invite'),
+# We use /people/[query] to automatically load the people list view
+# and search for query. There are two reserved urls, r'/people/me/?'
+# and r'/people/invite/?' which we want to point to
+# 'profiles_view_my_profile' and 'profiles_invite' accordingly. If a
+# user hits these urls without a trailing / we redirect him
+# permanently to the url with the trailing slash, to avoid reaching
+# the people list view with 'me' or 'invite' as a search term.
+urlpatterns = patterns('',
+    url(r'^me/$', 'remo.profiles.views.view_my_profile',
+        name='profiles_view_my_profile'),
+    url(r'^me$', 'django.views.generic.simple.redirect_to',
+        {'url': 'me/', 'permanent': True}),
+    url(r'^invite/$', 'remo.profiles.views.invite', name='profiles_invite'),
+    url(r'^invite$', 'django.views.generic.simple.redirect_to',
+        {'url': 'invite/', 'permanent': True}),
     # This url is intentionally left without a $
-    url(r'^', 'list_profiles', name='profiles_list_profiles'),
+    url(r'^', 'remo.profiles.views.list_profiles',
+        name='profiles_list_profiles'),
 )
