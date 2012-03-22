654fd0d62374c12d7523bb24828e1e4975addbdd
Pierros Papadeas <ppapadeas@gmail.com>
2012-03-22 22:31:35 +0200

    Added action buttons and fixed js errors

--- media/js/dashboard.js
+++ media/js/dashboard.js
@@ -1,51 +1,52 @@
-$(document).ready( function () {
-    $("#dashboard-br-all-button").click(function() {
+$(document).ready(function () {
+    $("#dashboard-br-all-button").click(function () {
         $(this).parent().addClass("active");
-        $("#dashboard-br-my-block").hide();
+        $("#dashboard-br-my-block").hide('fast');
         $("#dashboard-br-mine-button").parent().removeClass("active");
-        $("#dashboard-br-mentees-block").hide();
+        $("#dashboard-br-mentees-block").hide('fast');
         $("#dashboard-br-mentees-button").parent().removeClass("active");
-        $("#dashboard-br-all-block").show();
+        $("#dashboard-br-all-block").show('fast');
     });
-    $("#dashboard-br-mine-button").click(function() {
+    $("#dashboard-br-mine-button").click(function () {
         $(this).parent().addClass("active");
-        $("#dashboard-br-my-block").show();
-        $("#dashboard-br-mentees-block").hide();
+        $("#dashboard-br-my-block").show('fast');
+        $("#dashboard-br-mentees-block").hide('fast');
         $("#dashboard-br-mentees-button").parent().removeClass("active");
         $("#dashboard-br-all-block").hide().parent().removeClass("active");
         $("#dashboard-br-all-button").parent().removeClass("active");
     });
-    $("#dashboard-br-mentees-button").click(function() {
+    $("#dashboard-br-mentees-button").click(function () {
         $(this).parent().addClass("active");
-        $("#dashboard-br-my-block").hide();
+        $("#dashboard-br-my-block").hide('fast');
         $("#dashboard-br-mine-button").parent().removeClass("active");
-        $("#dashboard-br-mentees-block").show();
-        $("#dashboard-br-all-block").hide();
+        $("#dashboard-br-mentees-block").show('fast');
+        $("#dashboard-br-all-block").hide('fast');
         $("#dashboard-br-all-button").parent().removeClass("active");
     });
 
-    $("#dashboard-sr-all-button").click(function() {
+    $("#dashboard-sr-all-button").click(function () {
         $(this).parent().addClass("active");
-        $("#dashboard-sr-my-block").hide();
+        $("#dashboard-sr-my-block").hide('fast');
         $("#dashboard-sr-mine-button").parent().removeClass("active");
-        $("#dashboard-sr-mentees-block").hide();
+        $("#dashboard-sr-mentees-block").hide('fast');
         $("#dashboard-sr-mentees-button").parent().removeClass("active");
-        $("#dashboard-sr-all-block").show();
+        $("#dashboard-sr-all-block").show('fast');
     });
-    $("#dashboard-sr-mine-button").click(function() {
+    $("#dashboard-sr-mine-button").click(function () {
         $(this).parent().addClass("active");
-        $("#dashboard-sr-my-block").show();
-        $("#dashboard-sr-mentees-block").hide();
+        $("#dashboard-sr-my-block").show('fast');
+        $("#dashboard-sr-mentees-block").hide('fast');
         $("#dashboard-sr-mentees-button").parent().removeClass("active");
         $("#dashboard-sr-all-block").hide().parent().removeClass("active");
         $("#dashboard-sr-all-button").parent().removeClass("active");
     });
-    $("#dashboard-sr-mentees-button").click(function() {
+    $("#dashboard-sr-mentees-button").click(function () {
         $(this).parent().addClass("active");
-        $("#dashboard-sr-my-block").hide();
+        $("#dashboard-sr-my-block").hide('fast');
         $("#dashboard-sr-mine-button").parent().removeClass("active");
-        $("#dashboard-sr-mentees-block").show();
-        $("#dashboard-sr-all-block").hide();
+        $("#dashboard-sr-mentees-block").show('fast');
+        $("#dashboard-sr-all-block").hide('fast');
         $("#dashboard-sr-all-button").parent().removeClass("active");
     });
-})
+
+});
