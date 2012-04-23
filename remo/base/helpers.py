9d43e2e70265bf6c8d35bd1ec7d2e85e8ef31588
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-04-23 15:58:21 +0300

    Hide emails using MailHide for anonymous users. Fix bug 740439.

--- remo/base/helpers.py
+++ remo/base/helpers.py
@@ -1,8 +1,16 @@
+import base64
+import binascii
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.markup.templatetags import markup
 from jingo import register
 from jinja2 import Markup
+
+from Crypto.Cipher import AES
+
+AES_PADDING = 16
+AES_IV_LENGTH = 16
 
 
 @register.filter
@@ -28,6 +36,7 @@ def format_datetime(obj, type=None):
 
 @register.function
 def get_static_map_url(width, height, lon, lat, zoom=4):
+    """Return static map url."""
     api_key = settings.CLOUDMADE_API
     marker_id = settings.CLOUDMADE_MARKER_85
     URL = ('http://staticmaps.cloudmade.com/%(api_key)s/staticmap?'
@@ -36,3 +45,44 @@ def get_static_map_url(width, height, lon, lat, zoom=4):
     return URL % {'api_key': api_key, 'width': width, 'height': height,
                   'marker_id': marker_id, 'lat': lat, 'lon': lon,
                   'zoom': zoom}
+
+
+def pad_string(str, block_size):
+    """Pad string to reach block_size."""
+    numpad = block_size - (len(str) % block_size)
+    return str + numpad * chr(numpad)
+
+
+def enc_string(str):
+    """Return AES CBC encrypted string."""
+    key = binascii.unhexlify(settings.MAILHIDE_PRIV_KEY)
+    mode = AES.MODE_CBC
+    iv = '\000' * AES_IV_LENGTH
+    obj = AES.new(key, mode, iv)
+    return obj.encrypt(str)
+
+
+@register.filter
+def mailhide(value):
+    """Return MailHided email address.
+
+    Code from https://code.google.com/p/django-mailhide-filter
+    """
+    args = {}
+    padded_value = pad_string(value, AES_PADDING)
+    encrypted_value = enc_string(padded_value)
+
+    args['public_key'] = settings.MAILHIDE_PUB_KEY
+    args['encrypted_email'] = base64.urlsafe_b64encode(encrypted_value)
+    args['domain'] = value[value.index('@')+1:]
+    args['email'] = value[0]
+
+    result = ("""<a href="http://mailhide.recaptcha.net/d?k=%(public_key)s&"""
+              """c=%(encrypted_email)s" onclick="window.open('"""
+              """http://mailhide.recaptcha.net/d?k=%(public_key)s&"""
+              """c=%(encrypted_email)s', '', 'toolbar=0,scrollbars=0,"""
+              """location=0,statusbar=0,menubar=0,resizable=0,width=500,"""
+              """height=300'); return false;" title="Reveal this e-mail"""
+              """ address">%(email)s...@%(domain)s</a>""") % args
+
+    return Markup(result)
