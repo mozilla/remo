cdb1916347d3e8c51eb50f62a60ef64f25cb16a1
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-04-26 14:16:42 +0300

    Update Foundation to v2.2.1. Fix bug 735508.

--- media/js/app.js
+++ media/js/app.js
@@ -1,7 +1,6 @@
-/* Foundation v2.1.5 http://foundation.zurb.com */
+/* Foundation v2.2.1 http://foundation.zurb.com */
 
 /* Activate the featured Orbit reel */
-
 $(window).load(function () {
     $('#featured').orbit({
         bullets: true,
@@ -9,44 +8,46 @@ $(window).load(function () {
     });
 });
 
-$(document).ready(function () {
-
+jQuery(document).ready(function ($) {
     /* Use this js doc for all application specific JS */
-
     /* TABS --------------------------------- */
     /* Remove if you don't need :) */
 
     function activateTab($tab) {
-        var $activeTab = $tab.closest('dl').find('a.active'),
-            contentLocation = $tab.attr("href") + 'Tab';
+	var $activeTab = $tab.closest('dl').find('a.active'),
+	contentLocation = $tab.attr("href") + 'Tab';
 
-        //Make Tab Active
-        $activeTab.removeClass('active');
-        $tab.addClass('active');
+	// Strip off the current url that IE adds
+	contentLocation = contentLocation.replace(/^.+#/, '#');
 
-        //Show Tab Content
-        $(contentLocation).closest('.tabs-content').children('li').hide();
-        $(contentLocation).show();
+	//Make Tab Active
+	$activeTab.removeClass('active');
+	$tab.addClass('active');
+
+	//Show Tab Content
+	$(contentLocation).closest('.tabs-content').children('li').hide();
+	$(contentLocation).css('display', 'block');
     }
 
     $('dl.tabs').each(function () {
-        //Get all tabs
-        var tabs = $(this).children('dd').children('a');
-        tabs.click(function (e) {
-            activateTab($(this));
-        });
+	//Get all tabs
+	var tabs = $(this).children('dd').children('a');
+	tabs.click(function (e) {
+	    activateTab($(this));
+	});
     });
 
     if (window.location.hash) {
-        activateTab($('a[href="' + window.location.hash + '"]'));
+	activateTab($('a[href="' + window.location.hash + '"]'));
+	$.foundation.customForms.appendCustomMarkup();
     }
 
     /* ALERT BOXES ------------ */
-    $(".alert-box").delegate("a.close", "click", function (event) {
-        event.preventDefault();
-        $(this).closest(".alert-box").fadeOut(function (event) {
-            $(this).remove();
-        });
+    $(".alert-box").delegate("a.close", "click", function(event) {
+	event.preventDefault();
+	$(this).closest(".alert-box").fadeOut(function(event){
+	    $(this).remove();
+	});
     });
 
 
@@ -54,6 +55,9 @@ $(document).ready(function () {
     /* Remove this and jquery.placeholder.min.js if you don't need :) */
 
     $('input, textarea').placeholder();
+
+    /* TOOLTIPS ------------ */
+    $(this).tooltips();
 
 
 
@@ -68,39 +72,38 @@ $(document).ready(function () {
     /* DROPDOWN NAV ------------- */
 
     var lockNavBar = false;
-    $('.nav-bar a.flyout-toggle').live('click', function (e) {
-        e.preventDefault();
-        var flyout = $(this).siblings('.flyout');
-        if (lockNavBar === false) {
-            $('.nav-bar .flyout').not(flyout).slideUp(500);
-            flyout.slideToggle(500, function () {
-                lockNavBar = false;
-            });
-        }
-        lockNavBar = true;
+    $('.nav-bar a.flyout-toggle').live('click', function(e) {
+	e.preventDefault();
+	var flyout = $(this).siblings('.flyout');
+	if (lockNavBar === false) {
+	    $('.nav-bar .flyout').not(flyout).slideUp(500);
+	    flyout.slideToggle(500, function(){
+		lockNavBar = false;
+	    });
+	}
+	lockNavBar = true;
     });
     if (Modernizr.touch) {
-        $('.nav-bar>li.has-flyout>a.main').css({
-            'padding-right' : '75px'
-        });
-        $('.nav-bar>li.has-flyout>a.flyout-toggle').css({
-            'border-left' : '1px dashed #eee'
-        });
+	$('.nav-bar>li.has-flyout>a.main').css({
+	    'padding-right' : '75px'
+	});
+	$('.nav-bar>li.has-flyout>a.flyout-toggle').css({
+	    'border-left' : '1px dashed #eee'
+	});
     } else {
-        $('.nav-bar>li.has-flyout').hover(function () {
-            $(this).children('.flyout').show();
-        }, function () {
-            $(this).children('.flyout').hide();
-        });
+	$('.nav-bar>li.has-flyout').hover(function() {
+	    $(this).children('.flyout').show();
+	}, function() {
+	    $(this).children('.flyout').hide();
+	})
     }
 
 
     /* DISABLED BUTTONS ------------- */
     /* Gives elements with a class of 'disabled' a return: false; */
-
-
     // auto fade out success messages
     $(".success").delay(5000).fadeOut('slow');
+
 });
 
 // leaflet root
