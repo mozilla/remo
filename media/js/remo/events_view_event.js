13a48adefd22daf8d99f09fee47cfb6112352fa8
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-07-23 16:26:30 +0300

    [fix bug 776479] Don't show flickr mashup if no hashtag.

--- media/js/remo/events_view_event.js
+++ media/js/remo/events_view_event.js
@@ -70,47 +70,50 @@ function initialize_mashup() {
     // photos we also try to load related tweets.
     //
     var flickr_mashup = $('#flickr-mashup');
-    flickr_mashup.flickrfeed('', flickr_mashup.data('hashtag').substr(1), {
-        limit: 20,
-        title: false,
-        date: false,
-        header: false,
-        randomize: true
-    }).bind('loaded', function() {
-        // Check if images exist
-        if ($('.flickrRow').length === 0) {
-            // No images, don't display tweets either.
-            return;
-        }
-        flickr_mashup.show();
-
-        $('#tweet-mashup').tweet({
-            query: $('#tweet-mashup').data('hashtag'),
-            page: 1,
-            avatar_size: 32,
-            count: 20,
-            loading_text: 'fetching tweets ...'
+    var hashtag = flickr_mashup.data('hashtag');
+    if (hashtag) {
+        flickr_mashup.flickrfeed('', flickr_mashup.data('hashtag').substr(1), {
+            limit: 20,
+            title: false,
+            date: false,
+            header: false,
+            randomize: true
         }).bind('loaded', function() {
-            var ul = $('ul.tweet_list');
-
-            // If we have tweets.
-            if (ul.find('li').length > 0) {
-                $('#tweet-mashup-container').show();
-                $('#tweet-mashup-fade').show();
-
-                ul.find('a').attr('target', '_blank');
-                var ticker = function() {
-                    setTimeout(function() {
-                        ul.find('li:first').animate( {marginTop: '-4em'}, 500, function() {
-                            $(this).detach().appendTo(ul).removeAttr('style');
-                        });
-                        ticker();
-                    }, 5000);
-                };
-                ticker();
+            // Check if images exist
+            if ($('.flickrRow').length === 0) {
+                // No images, don't display tweets either.
+                return;
             }
+            flickr_mashup.show();
+
+            $('#tweet-mashup').tweet({
+                query: $('#tweet-mashup').data('hashtag'),
+                page: 1,
+                avatar_size: 32,
+                count: 20,
+                loading_text: 'fetching tweets ...'
+            }).bind('loaded', function() {
+                var ul = $('ul.tweet_list');
+
+                // If we have tweets.
+                if (ul.find('li').length > 0) {
+                    $('#tweet-mashup-container').show();
+                    $('#tweet-mashup-fade').show();
+
+                    ul.find('a').attr('target', '_blank');
+                    var ticker = function() {
+                        setTimeout(function() {
+                            ul.find('li:first').animate( {marginTop: '-4em'}, 500, function() {
+                                $(this).detach().appendTo(ul).removeAttr('style');
+                            });
+                            ticker();
+                        }, 5000);
+                    };
+                    ticker();
+                }
+            });
         });
-    });
+    }
 }
 
 $(document).ready(function() {
