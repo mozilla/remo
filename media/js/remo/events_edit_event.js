963f2265744cb68e4801c9a5a0a3f77fd8e48bd3
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-07-24 17:23:04 +0300

    [fix bug 775433] Beautify date and time select in event edit.

--- media/js/remo/events_edit_event.js
+++ media/js/remo/events_edit_event.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+    // Move foundation elements to position.
+    ['start', 'end'].forEach(function(obj) {
+        ['0_month', '0_day', '0_year', '1_hour', '1_minute'].forEach(function(elem) {
+            var destination = $('#' + obj + '-' + elem.substr(2));
+            var form_elem = $('#id_' + obj + '_form_' + elem);
+            var foundation_elem = form_elem.next().detach();
+
+            form_elem.detach().appendTo(destination);
+            foundation_elem.appendTo(destination);
+        });
+    });
+
+    // Auto change end year, month, day when start changes.
+    ['month', 'day', 'year'].forEach(function(when) {
+        $('#id_start_form_0_' + when).change(function() {
+            var obj = $('#id_end_form_0_' + when);
+            obj.val($('#id_start_form_0_' + when).val());
+            obj.trigger('change');
+        });
+    });
+
     // Dynamically add Metric forms
     $('.metricblock').formset({
         formCssClass: 'dynamic-metric-form',
