1fb7e4fb36f3ef63894f42ad0da3d2a7620195ad
John Giannelos <johngiannelos@gmail.com>
2013-03-26 02:19:22 +0200

    [Fix bug 854453] Fix newline format in iCAL description field

--- remo/base/helpers.py
+++ remo/base/helpers.py
@@ -15,6 +15,8 @@ from Crypto.Cipher import AES
 
 AES_PADDING = 16
 AES_IV_LENGTH = 16
+LINE_LIMIT = 75
+FOLD_SEP = u'\r\n '
 
 
 @register.filter
@@ -193,3 +195,42 @@ def user_is_mentor(user):
 def user_is_admin(user):
     """Check if a user belongs to Admin's group."""
     return user.groups.filter(name='Admin').exists()
+
+
+@register.filter
+def ical_escape_char(text):
+    """Escape characters as defined in RFC5545.
+
+    Original code from https://github.com/collective/icalendar
+    Altered by John Giannelos <jgiannelos@mozilla.com>
+
+    """
+
+    return (text.replace('\N', '\n')
+                .replace('\\', '\\\\')
+                .replace(';', r'\;')
+                .replace(',', r'\,')
+                .replace('\r\n', r'\n')
+                .replace('\n', r'\n'))
+
+
+@register.filter
+def ical_format_lines(text):
+    """Make a string folded as defined in RFC5545.
+
+    Original code from https://github.com/collective/icalendar
+    Altered by John Giannelos <jgiannelos@mozilla.com>
+
+    """
+
+    ret_line = u''
+    byte_count = 0
+    for char in text:
+        char_byte_len = len(char.encode('utf-8'))
+        byte_count += char_byte_len
+        if byte_count >= LINE_LIMIT:
+            ret_line += FOLD_SEP
+            byte_count = char_byte_len
+        ret_line += char
+
+    return ret_line
