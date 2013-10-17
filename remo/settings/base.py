d626e664d4be9a8adbb16024835447c5b6aa014e
Giorgos Logiotatidis <giorgos@mozilla.com>
2013-10-17 11:35:20 +0300

    Correct reps-council mailing list address.

--- remo/settings/base.py
+++ remo/settings/base.py
@@ -119,7 +119,7 @@ CONTRIBUTE_URL = ('http://www.mozilla.org/contribute/'
 
 
 REPS_MENTORS_LIST = 'reps-mentors@lists.mozilla.org'
-REPS_COUNCIL_LIST = 'reps-council@mozilla.com'
+REPS_COUNCIL_LIST = 'reps-council@lists.mozilla.org'
 
 # Mozillians API
 MOZILLIANS_API_BASE = 'https://mozillians.org'
