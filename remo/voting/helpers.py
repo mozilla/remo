7c49281e50af23f5f9684c46670fea1ba9ca0866
b10n1k <jbonatakis@gmail.com>
2014-04-08 11:25:41 +0300

    [Fix bug 861993] Add progress bars in voting.

--- remo/voting/helpers.py
+++ remo/voting/helpers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from jingo import register
 
 
@@ -6,3 +8,22 @@ def get_users_voted(poll):
     """Return the number of users voted to the specific poll."""
 
     return poll.users_voted.all().count()
+
+
+def get_voters_per_group(poll):
+    """Return the number of users of a specific poll per group."""
+
+    return User.objects.filter(groups=poll.valid_groups).count()
+
+
+@register.filter
+def get_meter_value(poll):
+    """Return the actual value for the meter element."""
+
+    users_voted = float(get_users_voted(poll))
+    voters_group = float(get_voters_per_group(poll))
+    try:
+        val = (users_voted/voters_group)*100
+        return int(val)
+    except ZeroDivisionError:
+        return 0
