1fa00272ad815a6b7027e8ba846809a79a97370e
Tasos Katsoulas <akatsoulas@gmail.com>
2014-05-09 16:21:01 +0300

    Add django-import-export to admin interface.

--- remo/profiles/admin.py
+++ remo/profiles/admin.py
@@ -1,4 +1,3 @@
-from itertools import izip_longest
 from socket import error as socket_error
 
 from django.conf.urls.defaults import patterns, url
@@ -6,10 +5,10 @@ from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from django.utils.timezone import now
+from django.http import HttpResponseRedirect
 
-import csv
+from import_export import fields, resources
+from import_export.admin import ExportMixin
 from functools import update_wrapper
 
 from remo.profiles.models import (FunctionalArea, UserAvatar, UserProfile,
@@ -20,35 +19,15 @@ from remo.profiles.tasks import check_celery
 admin.site.unregister(User)
 
 
-def export_mentorship_csv(modeladmin, request, queryset):
-    """Export mentorship csv from admin site."""
-    today = now().strftime('%Y-%m-%d-%H:%M')
-    filename = 'mentorship-%s.csv' % today
-    response = HttpResponse(mimetype='text/csv')
-    response['Content-Disposition'] = ('attachment; filename="%s"' %
-                                       filename)
-    mentorship = []
-
-    for user in queryset.filter(groups__name='Mentor'):
-        column = [user.get_full_name().encode('utf-8')]
-        column += [u.user.get_full_name().encode('utf-8')
-                   for u in user.mentees.all()]
-        mentorship.append(column)
-    writer = csv.writer(response)
-    writer.writerows(izip_longest(*mentorship, fillvalue=None))
-    return response
-
-
 class UserProfileInline(admin.StackedInline):
     """ReportLink Inline."""
     model = UserProfile
     fk_name = 'user'
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(ExportMixin, UserAdmin):
     """User Admin."""
     inlines = [UserProfileInline]
-    actions = [export_mentorship_csv]
 
     def get_urls(self):
         """Return custom and UserAdmin urls."""
@@ -81,15 +60,26 @@ class UserAdmin(UserAdmin):
             return HttpResponseRedirect(reverse('admin:auth_user_changelist'))
 
 
-class UserAvatarAdmin(admin.ModelAdmin):
+class UserAvatarResource(resources.ModelResource):
+    user = fields.Field()
+
+    class Meta:
+        model = UserAvatar
+
+    def dehydrate_user(self, useravatar):
+        return useravatar.user.get_full_name()
+
+
+class UserAvatarAdmin(ExportMixin, admin.ModelAdmin):
     """UserAvatar Admin."""
+    resource_class = UserAvatarResource
     list_display = ('display_name', 'avatar_url', 'last_update')
 
     def display_name(self, obj):
         return obj.user.userprofile.display_name
 
 
-class FunctionalAreaAdmin(admin.ModelAdmin):
+class FunctionalAreaAdmin(ExportMixin, admin.ModelAdmin):
     """FunctionalArea Admin."""
     list_display = ('name', 'registered_reps', 'registered_mozillians',
                     'registered_events', 'active')
@@ -107,8 +97,25 @@ class FunctionalAreaAdmin(admin.ModelAdmin):
         return self.model.objects.get_query_set()
 
 
-class UserStatusAdmin(admin.ModelAdmin):
+class UserStatusResource(resources.ModelResource):
+    user = fields.Field()
+    replacement_rep = fields.Field()
+
+    class Meta:
+        model = UserStatus
+
+    def dehydrate_user(self, userstatus):
+        return userstatus.user.get_full_name()
+
+    def dehydrate_replacement_rep(self, userstatus):
+        if userstatus.replacement_rep:
+            return userstatus.replacement_rep.get_full_name()
+        return None
+
+
+class UserStatusAdmin(ExportMixin, admin.ModelAdmin):
     """User Status Admin."""
+    resource_class = UserStatusResource
     model = UserStatus
     list_display = ('user', 'expected_date', 'return_date', 'created_on',
                     'replacement_rep')
