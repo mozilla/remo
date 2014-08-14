5e20ecb362ab2d2cc9dc9f1f6783d4ee8a164691
Tasos Katsoulas <akatsoulas@gmail.com>
2014-08-14 15:08:58 +0300

    [fix bug 1053052] Add an EventResouce class to the admin panel.

--- remo/events/admin.py
+++ remo/events/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from import_export import fields, resources
 from import_export.admin import ExportMixin
 from models import (Attendance, Event, EventGoal, EventMetric,
                     EventMetricOutcome)
@@ -15,8 +16,30 @@ class EventGoalAdmin(ExportMixin, admin.ModelAdmin):
     model = EventGoal
 
 
+class EventResource(resources.ModelResource):
+    event_goals = fields.Field()
+    event_categories = fields.Field()
+
+    class Meta:
+        model = Event
+        exclude = ('id', 'categories', 'goals',)
+
+    def dehydrate_event_goals(self, event):
+        if event.goals.all().exists():
+            goals = ', '.join(x.name for x in event.goals.all())
+            return goals
+        return ''
+
+    def dehydrate_event_categories(self, event):
+        if event.categories.all().exists():
+            categories = ', '.join([x.name for x in event.categories.all()])
+            return categories
+        return ''
+
+
 class EventAdmin(ExportMixin, admin.ModelAdmin):
     """Event Admin."""
+    resource_class = EventResource
     inlines = [AttendanceInline]
     model = Event
     list_display = ('name', 'start', 'end')
