6a1245b373975aff7dbd4a38567325b437fb6b0f
John Giannelos <johngiannelos@gmail.com>
2015-03-05 17:28:53 +0200

    [Fix bug 1139878] Change report form fields to facilitate "initiatives".
    * ChoiceField instead ModelMultipleChoiceField for contribution areas
    * Campaign is now a required field
    * Removed CSS/JS code that handled campaign show/hide

--- remo/reports/forms.py
+++ remo/reports/forms.py
@@ -6,7 +6,7 @@ from django.utils.timezone import now
 import happyforms
 
 from remo.profiles.models import FunctionalArea
-from remo.reports import ACTIVITY_CAMPAIGN, UNLISTED_ACTIVITIES
+from remo.reports import UNLISTED_ACTIVITIES
 from remo.reports.models import Activity, Campaign, NGReport, NGReportComment
 
 
@@ -15,41 +15,33 @@ class NGReportForm(happyforms.ModelForm):
     report_date = forms.DateField(input_formats=['%d %B %Y'])
     activity = forms.ModelChoiceField(
         queryset=Activity.active_objects.exclude(name__in=UNLISTED_ACTIVITIES))
-    campaign = forms.ModelChoiceField(queryset=Campaign.active_objects.all(),
-                                      required=False)
-    functional_areas = forms.ModelMultipleChoiceField(
-        queryset=FunctionalArea.active_objects.all())
+    campaign = forms.ModelChoiceField(queryset=Campaign.active_objects.all())
+    functional_areas = forms.ChoiceField()
 
     def __init__(self, *args, **kwargs):
         """ Initialize form.
 
         Dynamically set the report date.
         """
+
         super(NGReportForm, self).__init__(*args, **kwargs)
         self.fields['activity'].empty_label = 'Please select an activity.'
         self.fields['activity'].error_messages['required'] = (
             'Please select an option from the list.')
-        self.fields['campaign'].empty_label = 'Please select a campaign.'
+        self.fields['campaign'].empty_label = 'Please select an initiative.'
+        area_choices = FunctionalArea.active_objects.values_list('id', 'name')
+        self.fields['functional_areas'].choices = area_choices
 
         # Dynamic functional_areas field
         if self.instance.id:
             current_areas = self.instance.functional_areas.all()
             query = Q(active=True) | Q(id__in=current_areas)
             areas = FunctionalArea.objects.filter(query)
-            self.fields['functional_areas'].queryset = areas
-
-    def clean(self):
-        """Clean Form."""
-        super(NGReportForm, self).clean()
-        cdata = self.cleaned_data
-
-        activity = cdata.get('activity')
-        if ((activity and activity.name == ACTIVITY_CAMPAIGN and
-             not cdata.get('campaign'))):
-            msg = 'Please select an option from the list.'
-            self._errors['campaign'] = self.error_class([msg])
-
-        return cdata
+            choices = areas.values_list('id', 'name')
+            if self.instance.functional_areas.exists():
+                initial_category = self.instance.functional_areas.all()[0]
+            self.fields['functional_areas'].choices = choices
+            self.fields['functional_areas'].initial = initial_category.id
 
     def clean_report_date(self):
         """Clean report_date field."""
@@ -57,14 +49,27 @@ class NGReportForm(happyforms.ModelForm):
             raise ValidationError('Report date cannot be in the future.')
         return self.cleaned_data['report_date']
 
+    def save(self, *args, **kwargs):
+        instance = super(NGReportForm, self).save(*args, **kwargs)
+
+        # Manually save functional areas.
+        # For compatibility reasons we are keeping functional areas
+        # as m2m in the models but we are forcing through this form a
+        # single entry in the m2m relation.
+        area_pk = self.cleaned_data['functional_areas']
+        area = FunctionalArea.objects.get(id=area_pk)
+        instance.functional_areas.clear()
+        instance.functional_areas.add(area)
+
+        return instance
+
     class Meta:
         model = NGReport
         fields = ['report_date', 'activity', 'campaign',
-                  'functional_areas', 'longitude', 'latitude', 'location',
+                  'longitude', 'latitude', 'location',
                   'link', 'link_description', 'activity_description']
         widgets = {'longitude': forms.HiddenInput(),
-                   'latitude': forms.HiddenInput(),
-                   'functional_areas': forms.SelectMultiple()}
+                   'latitude': forms.HiddenInput()}
 
 
 class NGVerifyReportForm(happyforms.ModelForm):
