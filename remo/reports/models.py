659915b4b78ab0313651c599dbd9da8cc1287db6
John Giannelos <johngiannelos@gmail.com>
2015-04-08 14:43:46 +0300

    Add missing import.

--- remo/reports/models.py
+++ remo/reports/models.py
@@ -16,7 +16,7 @@ import remo.base.utils as utils
 from remo.base.utils import add_permissions_to_groups
 from remo.base.models import GenericActiveManager
 from remo.base.tasks import send_remo_mail
-from remo.base.utils import daterange, get_date
+from remo.base.utils import daterange, get_date, get_object_or_none
 from remo.events.models import Attendance as EventAttendance, Event
 from remo.profiles.models import FunctionalArea
 from remo.reports import (ACTIVITY_CAMPAIGN, ACTIVITY_EVENT_ATTEND,
