eb7e7984d814d5a27ca50f67a3f5b70216b36ec1
Yousef Alam <flamingspaz96@googlemail.com>
2017-03-14 16:47:41 +0000

    Remove Git files from prod images

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,5 +3,6 @@ set -e
 
 export MESOS_CLUSTER=True
 export `cat remo/settings/env-dist | sed s/\ =\ /=/ | grep -v ^\# | xargs`
+rm -rf .git
 python manage.py collectstatic --noinput
 python manage.py compress --force --engine jinja2 --extension=.jinja
