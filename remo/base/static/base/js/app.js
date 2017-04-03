f115b8e4a3092e4367f34e8e37dce0dbde6fdd8f
tsmrachel <t.s.m.rachel@gmail.com>
2017-04-04 00:15:56 +0800

    [fix bug 1138386] Group top menu links (#1323)
    * [fix bug 1138386] Group top menu links
    
    * removed left padding and refactored #navigation-box css
    
    * refactored .dropdown and .dropbtn

--- remo/base/static/base/js/app.js
+++ remo/base/static/base/js/app.js
@@ -41,33 +41,6 @@ jQuery(document).ready(function ($) {
     /* Remove this and jquery.placeholder.min.js if you don't need :) */
     $('input, textarea').placeholder();
 
-    /* DROPDOWN NAV ------------- */
-    var lockNavBar = false;
-    $('.nav-bar a.flyout-toggle').on('click', function(e) {
-        e.preventDefault();
-        var flyout = $(this).siblings('.flyout');
-        if (!lockNavBar) {
-            $('.nav-bar .flyout').not(flyout).slideUp(500);
-            flyout.slideToggle(500, function(){
-                lockNavBar = false;
-            });
-        }
-        lockNavBar = true;
-    });
-    if (Modernizr.touch) {
-        $('.nav-bar>li.has-flyout>a.main').css({
-            'padding-right' : '75px'
-        });
-        $('.nav-bar>li.has-flyout>a.flyout-toggle').css({
-            'border-left' : '1px dashed #eee'
-        });
-    } else {
-        $('.nav-bar>li.has-flyout').hover(function() {
-            $(this).children('.flyout').show();
-        }, function() {
-            $(this).children('.flyout').hide();
-        });
-    }
 
     /* DISABLED BUTTONS ------------- */
     /* Gives elements with a class of 'disabled' a return: false; */
