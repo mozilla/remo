b40711c34da552eeedeb75eb02eb37e3999a5daf
Tasos Katsoulas <akatsoulas@gmail.com>
2017-09-19 16:58:59 +0300

    Run migrate with 'noinput' argument.

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py compress --engine jinja2 --extension=.jinja
 gunicorn remo.wsgi:application -w 3 -b 0.0.0.0:${PORT:-8000} --log-file -
