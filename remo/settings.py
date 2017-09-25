58adf7327247b9ac187fb0652facd2b1adb6b7ca
Yousef Alam <flamingspaz96@googlemail.com>
2017-09-25 14:31:03 +0100

    Add fanout prefix setting for celery

--- remo/settings.py
+++ remo/settings.py
@@ -169,7 +169,8 @@ REDIS_CONNECT_RETRY = config('REDIS_CONNECT_RETRY',
 BROKER_URL = config('CELERY_BROKER_URL', default='amqp://guest:guest@broker:5672//')
 if BROKER_URL.startswith('redis'):
     BROKER_TRANSPORT_OPTIONS = {
-        'visibility_timeout': config('REDIS_VISIBILITY_TIMEOUT', default=604800, cast=int)
+        'visibility_timeout': config('REDIS_VISIBILITY_TIMEOUT', default=604800, cast=int),
+        'fanout_prefix': True
     }
 
 # Mapbox
