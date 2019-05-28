2c468f3a9acd2704790fdadf2c48eda9dc82dae8
Michael Kohler <me@michaelkohler.info>
2019-05-28 20:56:41 +0200

    Bug 1446652 - Remove IE Support

--- remo/voting/static/voting/js/progress.js
+++
@@ -1,8 +0,0 @@
-/* This script fix the progress bar for IE in case of overload */
-$(document).ready(function() {
-    $('.progressspan').each( function(i) {
-        var $this = $(this);
-        var percent = Math.min($this.data('percent'), 100);
-        $this.css('width', percent + '%');
-    });
-});
