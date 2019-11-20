116c3be441ab68af1ca1221d22232a21a6c551f6
John Giannelos <jgiannelos@mozilla.com>
2019-11-20 11:43:45 +0100

    Force HTTPS redirection in Django level

--- remo/settings.py
+++ remo/settings.py
@@ -102,6 +102,7 @@ SECURE_BROWSER_XSS_FILTER = config('SECURE_BROWSER_XSS_FILTER', default=True, ca
 SECURE_HSTS_INCLUDE_SUBDOMAINS = config('SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True, cast=bool)
 SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', default=31536000, cast=int)  # 1 year
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=True, cast=bool)
 
 # Project emails
 REPS_MENTORS_LIST = config('REPS_MENTORS_LIST', default='reps-mentors@lists.mozilla.org')
